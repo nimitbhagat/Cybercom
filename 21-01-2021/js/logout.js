4dffa8a9d8a09b713cb9e3a8f7e6b83dfa6ad5da
Nimit Bhagat <bhagatnimit8@gmail.com>
2021-01-21 17:54:52 +0530

    5:54pm

--- 21-01-2021/js/logout.js
+++ 21-01-2021/js/logout.js
@@ -1,14 +1,26 @@
 function logout() {
+
+
+    var users = [];
+
+    if (localStorage.getItem('users')) {
+        users = JSON.parse(localStorage.getItem('users'));
+    }
+
+
+
+    var d = new Date();
+    var date = d.getDate() + "/" + d.getMonth() + 1 + "/" + d.getFullYear();
+    var time = d.getHours() + ":" + d.getUTCMinutes() + ":" + d.getSeconds();
+
+
+    users[sessionStorage.getItem("id")].lout = date + " " + time;
+    localStorage.setItem("users", JSON.stringify(users));
+
+    //alert(users[user].lin);
+
+
     sessionStorage.clear();
     window.location.replace("/login.html");
 
-
-    var loginSession = [];
-
-
-    if (localStorage.getItem('logs')) {
-        loginSession = JSON.parse(localStorage.getItem('logs'));
-    }
-
-    localStorage
 }
